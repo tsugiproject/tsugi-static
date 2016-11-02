4f2e9d5060245f9bc97d11f8e9ff675b9648e1c9
Charles Severance <csev@umich.edu>
2016-11-02 17:20:41 -0400

    Make it so only one player is running at the same time.

--- js/tsugiscripts.js
+++ js/tsugiscripts.js
@@ -252,10 +252,22 @@ function labnolThumb(id) {
 }
  
 function labnolIframe() {
+    // Reset any currently active players...
+    var v = document.getElementsByClassName("generated-youtube-frame");
+    for (n = 0; n < v.length; n++) {
+        div = document.createElement("div");
+        div.setAttribute("data-id", v[n].dataset.id);
+        div.innerHTML = labnolThumb(v[n].dataset.id);
+        div.onclick = labnolIframe;
+        v[n].parentNode.replaceChild(div, v[n]);
+    }
+
     var iframe = document.createElement("iframe");
     var embed = "https://www.youtube.com/embed/ID?autoplay=1";
     iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
     iframe.setAttribute("frameborder", "0");
+    iframe.setAttribute("class", "generated-youtube-frame");
+    iframe.setAttribute("data-id", this.dataset.id);
     iframe.setAttribute("allowfullscreen", "1");
     iframe.setAttribute("webkitAllowFullScreen", "1");
     iframe.setAttribute("mozallowfullscreen", "1");
