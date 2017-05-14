cba9791e0e82523f39bc3972cb7938f44f3e63f7
Charles Severance <csev@umich.edu>
2017-05-14 13:10:15 -0400

    Add tsugiSetCookie

--- js/tsugiscripts.js
+++ js/tsugiscripts.js
@@ -296,3 +296,13 @@ function tsugiHandlebarsToDiv(div, name, context) {
     $('#'+div).empty().append(tsugiHandlebarsRender(name, context));
 }
 
+
+// Straight outta W3Schools
+// https://www.w3schools.com/js/js_cookies.asp
+function tsugiSetCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
