03c26bb2a3f07171fe732dabe52f19629b515db5
Charles Severance <csev@umich.edu>
2017-08-31 14:42:34 -0400

    Move poly fill of web components to the bottom.

--- js/tsugiscripts.js
+++ js/tsugiscripts.js
@@ -365,4 +365,32 @@ function tsugiEmbedKeep() {
     $TSUGI_EMBED_TIMEOUT = false;
 }
 
+// Make sure to polyfill web component capabilities
+// https://www.webcomponents.org/polyfills
+if ( 'registerElement' in document
+      && 'import' in document.createElement('link')
+      && 'content' in document.createElement('template')) {
+    // platform is good!
+    // console.log("Web Components there ... "+_TSUGI.staticroot);
+} else {
+    var polyfill = _TSUGI.staticroot+'/polyfill/webcomponentsjs-1.0.5/webcomponents-lite.js'
+    var e = document.createElement('script');
+    e.src = polyfill;
+    document.body.appendChild(e);
+    console.log("Polyfill web components.. "+polyfill);
+}
+
+// Make sure to polyfill fetch() if needed
+// https://github.com/github/fetch
+
+if (window.fetch) {
+    // console.log("Fetch is there...");
+} else {
+    // polyfill fetch()
+    var polyfill = _TSUGI.staticroot+'/polyfill/fetch-2.0.3/fetch.js'
+    var e = document.createElement('script');
+    e.src = polyfill;
+    document.body.appendChild(e);
+    console.log("Polyfill fetch.. "+polyfill);
+}
 
--- js/tsugiscripts_head.js
+++ js/tsugiscripts_head.js
@@ -32,29 +32,3 @@ function inIframe () {
     }
 }
 
-// Make sure to polyfill web component capabilities
-// https://www.webcomponents.org/polyfills
-if (false && 'registerElement' in document
-      && 'import' in document.createElement('link')
-      && 'content' in document.createElement('template')) {
-    // platform is good!
-    // console.log("All good... "+_TSUGI.staticroot);
-} else {
-    // polyfill the platform!
-    var polyfill = _TSUGI.staticroot+'/polyfill/webcomponentsjs-1.0.5/webcomponents-lite.js'
-    // Make this the next to load...
-    document.write('<scr'+'ipt type="text/javascript" src="'+polyfill+'"></scr'+'ipt>');
-    console.log("Polyfill web components.. "+polyfill);
-}
-
-// Make sure to polyfill fetch() if needed
-// https://github.com/github/fetch
-
-if (window.fetch) {
-    // console.log("Fetch is already there...");
-} else {
-    // polyfill fetch()
-    var polyfill = _TSUGI.staticroot+'/polyfill/fetch-2.0.3/fetch.js'
-    document.write('<scr'+'ipt type="text/javascript" src="'+polyfill+'"></scr'+'ipt>');
-    console.log("Polyfill fetch.. "+polyfill);
-}
