2c3ca3c55b055c0f63951a60f1d6280f04cb3b18
Charles Severance <csev@umich.edu>
2019-11-02 23:30:47 -0400

    Add clipboard copy

--- js/tsugiscripts.js
+++ js/tsugiscripts.js
@@ -634,3 +634,18 @@ function tsugiSha256(ascii) {
     }
     return result;
 }
+
+function tsugiCopyText(id) {
+  /* Get the text field */
+  var copyText = document.getElementById(id);
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  console.log("Copied the text: " + copyText.value);
+}
